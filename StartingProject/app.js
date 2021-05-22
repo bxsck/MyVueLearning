271320f3f5005cc0cea9981eaae9b53d643c320d
bxsck <bosstk05nya@gmail.com>
2021-05-22 23:31:25 +0700

    change from  vanillaJS to vueJS

--- StartingProject/app.js
+++ StartingProject/app.js
@@ -1,4 +1,20 @@
-const buttonElement = document.querySelector("button");
+Vue.createApp({
+    data(){
+        return{
+          goals:[],
+          enteredValue:'',
+
+        };
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue);
+            this.enteredValue = ''
+        }
+    }
+}).mount('#app');
+
+/* const buttonElement = document.querySelector("button");
 const inputElement = document.querySelector("input");
 const listElement = document.querySelector("ul");
 
@@ -11,3 +27,4 @@ const addGoal = () => {
 };
 
 buttonElement.addEventListener("click", addGoal);
+ */
