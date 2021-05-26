4c95f9c9720afd7da599eef03fe62d258b00ebce
bxsck <bosstk05nya@gmail.com>
2021-05-26 20:56:36 +0700

    vue create app

---
+++ my-first-vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "bosck",
+          name: "Teerapat Surasin",
+          phone: "0123456789",
+          email: "bosck@bosck.com",
+        },
+        {
+          id: "mook",
+          name: "mookmanee mmk",
+          phone: "0123456789",
+          email: "mook@bosck.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ my-first-vue/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click='toggleDetails'>Show Details</button>
+    <u v-if='detailsAreVisible'>
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </u>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "bosck",
+        name: "Teerapat Surasin",
+        phone: "0123456789",
+        email: "bosck@bosck.com",
+      },
+    };
+  },
+  methods:{
+      toggleDetails(){
+          this.detailsAreVisible = !this.detailsAreVisible;
+      }
+  }
+};
+</script>
