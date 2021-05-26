3dca68ddb830388c689da0a89027fc69482e1fba
bxsck <bosstk05nya@gmail.com>
2021-05-26 22:19:21 +0700

    add dynamic text

--- my-first-vue/src/components/FriendContact.vue
+++ my-first-vue/src/components/FriendContact.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <h2>{{ friend.name }}</h2>
-    <button @click='toggleDetails'>Show Details</button>
+    <button @click="toogleDetails">{‌{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <u v-if='detailsAreVisible'>
       <li><strong>Phone:</strong> {{ friend.phone }}</li>
       <li><strong>Email:</strong> {{ friend.email }}</li>
