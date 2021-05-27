24b71ccfa16b4672773cb892470aba421ef9553b
bxsck <bosstk05nya@gmail.com>
2021-05-27 14:25:23 +0700

    update components interaction

---
+++ PropsAndCustomEvents/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data @set-data='setUserData'></user-data>
+  </div>
+</template>
+
+<script>
+import ActiveUser from "./components/ActiveUser.vue";
+export default {
+  components: { ActiveUser },
+  data() {
+    return {
+      user: {
+        name: "Teerapat Surasin",
+        age: 22,
+      },
+    };
+  },
+  methods: {
+      setUserData(name, age){
+          this.user ={
+              name: name,
+              age: +age
+          }
+      }
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 10px
+}
+</style>
---
+++ PropsAndCustomEvents/src/components/ActiveUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h2>{{ username }}</h2>
+    <h3>{{ userage }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userage: {
+      type: Number,
+      required: true,
+    },
+  },
+  /* props: ['username','userage'] */
+};
+</script>
+
+<style></style>
